5de90e6f5d4bc19af18ae07ec5db8052b3b39b62
Dood <doodwheresmycar@gmail.com>
2022-04-04 18:28:54 -0600

    Initial commit

---
+++ Client.py
@@ -0,0 +1,12 @@
+"""This is responsable for running the UI, tracking penalties and wait times, and talking to the server"""
+
+"""State, wait time, list of server nodes, alive bool, messaging info(IP, port, ID), pending move queue?"""
+
+def send_move(): # Send relivant data to
+    pass
+
+def receive(): # Respond to incoming log, may update leader info
+    pass
+
+def user_action(): # May need sub methods
+    pass
---
+++ Server.py
@@ -0,0 +1,29 @@
+
+"""has ID, current term, log, voted for, vector clock?, client and node data(IP, port, ID), timeout"""
+
+"""Need two threads to listen and send"""
+
+def time_out():  #$2
+    pass
+
+def crash(): #$2
+    pass
+
+def restart(): #$2
+    pass
+
+def request(item): # Send request to leader, maybe need
+    pass
+
+def update_log(items): # Given log items $$$$$$$$$$$$$$$
+    pass
+
+def upend_entries(): # For leader, appends entries, heartbeat $$$$$$$$$$$$$$
+    pass
+
+def request_vote(): #$$$$$$$$$$$$$$$$$
+    pass
+
+def talk_to_client():
+    pass
+
